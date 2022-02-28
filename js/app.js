f2e78ae472d38371fa054ace6f3a61ec412a081d
owakeelahmmed <mdowakeelahmed5678@gamail.com>
2022-03-01 01:59:52 +0600

    add to error handling

--- js/app.js
+++ js/app.js
@@ -4,23 +4,31 @@ const main = document.getElementById('main');
 const inputBtn = () => {
     const input = document.getElementById('input-value');
     const searchText = input.value;
+    const errorMessage = document.getElementById('error-text');
     
-
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    if(searchText == ''){
+         errorMessage.innerText = "Please enter a search text";
+         input.value='';
+         main.innerHTML = '';
+    }else{
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
         .then(response => response.json())
         .then(data => phoneDisplay(data.data))
+        input.value='';
+        main.innerHTML = '';
+        errorMessage.innerHTML = '';
+    }
+
 }
 
 
 const phoneDisplay = (phones) => {
-    
-
-
     for(const phone of phones){
         const div = document.createElement('div');
         console.log(phone);
 
         div.classList.add("col-lg-4");
+        div.classList.add("col-12");
         div.classList.add("mt-5")
         div.classList.add("mb-3")
         div.innerHTML = `
@@ -30,7 +38,7 @@ const phoneDisplay = (phones) => {
             <h5 class="card-title">${phone.brand}</h5>
             <p class="card-text">${phone.phone_name}</p>
             <p class="card-text">${phone.slug}</p>
-            <button onclick = "cardDetails('')" class="btn btn-primary">See More</button>
+            <button onclick = "cardDetails('${phone.brand}')" class="btn btn-primary">See More</button>
         </div>
     </div>
         
@@ -38,4 +46,13 @@ const phoneDisplay = (phones) => {
         main.appendChild(div)
 
     }
+}
+
+
+
+const cardDetails = (phone) => {
+
+
+
+
 }
