a8cff2ada8b4bf0389794befd50dd1f7c9809c5a
owakeelahmmed <mdowakeelahmed5678@gamail.com>
2022-03-01 15:16:57 +0600

    details card done

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ const inputBtn = () => {
          errorMessage.innerText = "Please enter a search text";
          input.value='';
          main.innerHTML = '';
-    }else{
+    }
+    else{
         fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
         .then(response => response.json())
         .then(data => phoneDisplay(data.data))
@@ -37,8 +38,7 @@ const phoneDisplay = (phones) => {
         <div class="card-body">
             <h5 class="card-title">${phone.brand}</h5>
             <p class="card-text">${phone.phone_name}</p>
-            <p class="card-text">${phone.slug}</p>
-            <button onclick = "cardDetails('${phone.brand}')" class="btn btn-primary">See More</button>
+            <button onclick = "cardDetails('${phone.slug}')" class="btn btn-primary">See More</button>
         </div>
     </div>
         
@@ -50,9 +50,46 @@ const phoneDisplay = (phones) => {
 
 
 
-const cardDetails = (phone) => {
+const cardDetails = (id) => {
+    console.log(id);
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const setDetails = (data.data);
+        console.log(setDetails);
 
 
 
+        const div = document.createElement('div');
+        main.innerHTML = "";
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${setDetails.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h6 class="card-title">${setDetails.brand}</h6>
+            <h6 class="card-title">${setDetails.mainFeatures.memory}</h6>
+            <h6 class="card-title">${setDetails.mainFeatures.displaySize}</h6>
+            <h6 class="card-title">${setDetails.mainFeatures.chipSet}</h6>
+            
+            </div>
+        </div>
+        `
+        main.appendChild(div);
 
+
+    });
 }
+
+
+
+
+
+
+
+// const setDetails = (info) => {
+
+// console.log(info);
+
+
+// }
