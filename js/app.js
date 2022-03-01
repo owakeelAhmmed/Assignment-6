b2135097f2f1d36623660c6edc3c822b2e34ba0b
owakeelahmmed <mdowakeelahmed5678@gamail.com>
2022-03-01 20:46:10 +0600

    finish datails

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-
 const main = document.getElementById('main');
 const inputBtn = () => {
     const input = document.getElementById('input-value');
@@ -21,19 +20,15 @@ const inputBtn = () => {
     }
 
 }
-
-
 const phoneDisplay = (phones) => {
     for(const phone of phones){
         const div = document.createElement('div');
-        console.log(phone);
-
         div.classList.add("col-lg-4");
         div.classList.add("col-12");
         div.classList.add("mt-5")
-        div.classList.add("mb-3")
+        div.classList.add("mb-2")
         div.innerHTML = `
-        <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+        <div class="card shadow p-3 bg-body rounded" style="width: 18rem;">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${phone.brand}</h5>
@@ -47,9 +42,6 @@ const phoneDisplay = (phones) => {
 
     }
 }
-
-
-
 const cardDetails = (id) => {
     console.log(id);
     const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
@@ -57,44 +49,35 @@ const cardDetails = (id) => {
     .then(response => response.json())
     .then(data => {
         const setDetails = (data.data);
-        console.log(setDetails);
-
-
-
         const div = document.createElement('div');
         main.innerHTML = "";
         div.classList.add("mb-3")
         div.innerHTML = `
-        <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
-        <div class="card-body">
-        <img style = "width:200px; height:200px;" src="${setDetails.image}" class="card-img-top" alt="...">
+            <div class="card mb-3 mt-4" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img style = "width:100%; height:100%; padding: 20px;" src="${setDetails.image}" class="card-img-top" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
                 <h2 class="card-title text-center text-success">${setDetails.brand}</h2>
                 <h6 class="card-title text-center text-danger">ReleaseDate:</h6>
-                <h6 class="card-title text-center">${setDetails.releaseDate}</h6>
+                <P class="card-title text-center">${setDetails.releaseDate}</P>
                 <h6 class="card-title text-center text-danger">Memory:</h6>
-                <h6 class="card-title text-center">${setDetails.mainFeatures.memory}</h6>
+                <P class="card-title text-center">${setDetails.mainFeatures.memory}</P>
                 <h6 class="card-title text-center text-danger">DisplaySize:</h6>
-                <h6 class="card-title text-center ">${setDetails.mainFeatures.displaySize}</h6>
+                <P class="card-title text-center ">${setDetails.mainFeatures.displaySize}</P>
                 <h6 class="card-title text-center text-danger">ChipSet:</h6>
-                <h6 class="card-title text-center">${setDetails.mainFeatures.chipSet}</h6>
+                <P class="card-title text-center">${setDetails.mainFeatures.chipSet}</P>
+                <h6 class="card-title text-center text-danger">Sensors:</h6>
+                <P class="card-title text-center">${setDetails.mainFeatures.sensors}</P>
+                </div>
+                </div>
             </div>
-        </div>
-        `
+            </div>
+                `
         main.appendChild(div);
-
-
     });
 }
 
 
-
-
-
-
-
-// const setDetails = (info) => {
-
-// console.log(info);
-
-
-// }
